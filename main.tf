f4fba2f9a01e3d86224041cdaba85eb51ba2c092
Atanyan, Vanush <VANUSH_ATANYAN@homedepot.com>
2024-01-31 20:09:06 +0400

    Init Git

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {}
+
+data "aws_ami" "latest_ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+data "aws_ami" "latest_amazon" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+}
+
+output "data_ubuntu_ami_id" {
+  value = data.aws_ami.latest_ubuntu.id
+}
+
+output "data_ubuntu_ami_name" {
+  value = data.aws_ami.latest_ubuntu.name
+}
+
+output "data_amazon_linux_ami_id" {
+  value = data.aws_ami.latest_amazon.id
+}
+
+output "data_amazon_linux_ami_name" {
+  value = data.aws_ami.latest_amazon.name
+}
