d91a6e4cdc28e947c67c8d82c349c83046ac86fe
Atanyan, Vanush <VANUSH_ATANYAN@homedepot.com>
2024-02-01 14:20:23 +0400

    Add Permissions To User to create roles and permissions

--- ecs-config/main.tf
+++ ecs-config/main.tf
@@ -52,8 +52,8 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment"
 resource "aws_ecs_task_definition" "main_task_definition" {
   family                   = "main-task-family"
   requires_compatibilities = ["FARGATE"]
-  network_mode             = "awsvpc" # Use awsvpc network mode for Fargate
-  cpu                      = "256"    # Specify CPU units (minimum value)
+  network_mode             = "awsvpc"
+  cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions = jsonencode([
