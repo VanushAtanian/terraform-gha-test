4800e3ae4fd0830d07d1f1b2d39d6f270af09956
Atanyan, Vanush <VANUSH_ATANYAN@homedepot.com>
2024-02-01 16:05:36 +0400

    Add routs and IGW

--- ecr-config/main.tf
+++ ecr-config/main.tf
@@ -40,6 +40,29 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "main_internet_gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_internet_gateway.id
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.main_subnet.id
+}
+
 resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
