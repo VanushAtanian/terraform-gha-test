7df08c7d2f5afda1bf540aa5346f73c91d04de3f
Atanyan, Vanush <VANUSH_ATANYAN@homedepot.com>
2024-02-01 04:55:09 +0400

    Export Credentials

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
     bucket     = "van-presentation-terraform-state"
     key        = "dev/presentation/terraform.tfstate"
     region     = "eu-central-1"
-    access_key = "AKIA2FRWNY5MJLC3L7GS"
-    secret_key = "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"
+    # access_key = "AKIA2FRWNY5MJLC3L7GS"
+    # secret_key = "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"
   }
 }
 
